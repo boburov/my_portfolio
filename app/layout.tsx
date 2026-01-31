ec516b15c4c6c7c71c7211b649855a8069528319
boburov <boburovdev@gmail.com>
2026-01-31 19:03:10 +0500

    fixed :D width height

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,8 +23,8 @@ export const metadata: Metadata = {
     images: [
       {
         url: "./boburov.png",
-        width: 1200,
-        height: 630,
+        width: 600,
+        height: 130,
         alt: "Shukurullo Boburov Portfolio",
       },
     ],
